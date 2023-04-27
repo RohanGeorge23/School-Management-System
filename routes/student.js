f285099b7be34ac43b5a3d60f4fcd3ebc2de55c5
RohanGeorge23 <rohangeorge77@gmail.com>
2023-04-27 21:14:19 +0530

    code cleanup

--- routes/student.js
+++ routes/student.js
@@ -5,10 +5,4 @@ const {allStudents,createStudent} = require('../controller/studentController')
 router.get('/all',allStudents)
 router.post('/create',createStudent)
 
-// Example Request Query and Parameter
-router.get('/rg/:id',(req,res)=>{
-    console.log(req.query)
-    res.send(req.params.id)
-})
-
 module.exports = router
