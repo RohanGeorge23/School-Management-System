41def3f4f536fc99667cd7b6e1275e677a6ac20e
RohanGeorge23 <rohangeorge77@gmail.com>
2023-04-28 10:58:40 +0530

    checking my .gitconfig file ssh

--- routes/student.js
+++ routes/student.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const {allStudents,createStudent} = require('../controller/studentController')
 
 // All Router Goes Here
+// Just commenting to check ssh 
 router.get('/all',allStudents)
 router.post('/create',createStudent)
 
